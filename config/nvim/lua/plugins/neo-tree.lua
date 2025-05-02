476e540e020879cf89fdbf1e035c8793929c1d8d
Oleg Plichko <oleg.plichko.fr@gmail.com>
2025-05-02 16:53:22 +0200

    split config and plugins, lualine

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -1,11 +1,4 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {{"nvim-treesitter", build = ":TSUpdate"}},
-  {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
@@ -17,5 +10,8 @@ return {
     opts = {
       -- fill any relevant options here
     },
+    config = function()
+      vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+    end
   }
-}
+ 
